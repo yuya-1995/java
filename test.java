2de469bfbbccce78d69d106a605b38a2bd88c7f8
佐々木裕哉 <sasa19951018@gmail.com>
2023-07-25 21:15:47 +0900

    import面白い

--- test.java
+++ test.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class test {
-    public static void main(String [] args) {
-      System.out.println("Hello Java!!!!!!");
+    public static void main(String[] args) {
+        Random ans = new Random(10);
+        Scanner num = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("0から9の数字を入力してください");
+            int userInput = num.nextInt();
+            int randomNumber = ans.nextInt(10);
+
+            if (userInput == randomNumber) {
+                System.out.println("あたり！");
+                break; // 当たった場合にループを抜ける
+            } else {
+                System.out.println("はずれ！");
+            }
+        }
+
+        num.close(); // Scannerをクローズ
+        System.out.println("game over");
     }
-  }
+}
