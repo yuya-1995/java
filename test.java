46e0d2cc9706d13acfd8cf68225623296e7a2cac
佐々木裕哉 <sasa19951018@gmail.com>
2023-07-26 09:49:45 +0900

    配列

--- test.java
+++ test.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class test {
-    public static void main(String [] args) {
-      System.out.println("Hello Java!!!!!!");
+    public static void main(String[] args) {
+        int[] numbers = {3, 4, 9};
+        Scanner user = new Scanner(System.in);
+        boolean found = false;
+
+        while (!found) {
+            System.out.println("数字を入力してください");
+            int input = user.nextInt();
+
+            for (int value : numbers) {
+                if (value == input) {
+                    System.out.println("あたり");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("はずれ");
+            }
+        }
+
+        user.close();
     }
-  }
+}
