ffcfd98470f635f29afbd7ef806ffc0ccdfbe0d9
佐々木裕哉 <sasa19951018@gmail.com>
2023-08-01 15:16:35 +0900

    SQL
    H2データベースを使用するには、次の手順を実行する必要があります：
    
    H2のJDBCドライバをダウンロードします。H2の公式ウェブサイト（http://www.h2database.com）からJARファイルを入手できます。通常はh2-xxxx.jarという名前のファイルです（xxxxはバージョン番号）。
    
    ダウンロードしたJARファイルをJavaのクラスパスに追加します。クラスパスに追加する方法はいくつかありますが、最も簡単な方法は、コードを実行するディレクトリにJARファイルを配置することです。
    
    クラスパスにJARファイルが配置されていることを確認してください。それが正しく配置されている場合、ClassNotFoundExceptionは解消され、H2のJDBCドライバが正しくロードされるはずです。

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.h2.Driver");
+        String dburl = "jdbc:h2:~/test";
+        String sql = "INSERT INTO EMPLOYEES(name) VALUES('aoki')";
+        Connection conn = DriverManager.getConnection(dburl);
+        conn.createStatement().executeUpdate(sql); // executeUpdate()を使用する
+        conn.close();
+    }
+}
